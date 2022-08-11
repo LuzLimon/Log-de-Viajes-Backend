05c663c7ec227fa976288cf34b6b2030e6a22e21
Luz Elena Limón de la Cruz <luzelenalimondelacruz123@gmail.com>
2022-08-11 15:34:12 -0500

    Fix: Repair Cors

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
-const cors = require('cors');
+const cors = require("cors");
 const mongoose = require('mongoose');
 const InitMongoose = require('../InitMongoose');
 require('dotenv').config();
@@ -13,11 +13,7 @@ InitMongoose(mongoose);
 
 app.use(morgan('common'));
 app.use(helmet());
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 //api routes
